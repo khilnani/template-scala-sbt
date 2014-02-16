221ed8a7bc86e5dd91e42c48260172e74382ec98
khilnani <nik@khilnani.org>
2014-02-15 20:23:06 -0500

    Moved scala-library to lib-scala to avoid conflict with compile time unmanaged libs in 'lib'

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ sbt clean package
 
 rm ./dist/*.jar
 cp ./target/**/name_*.jar ./dist
-cp -r ./lib/ ./dist/
+cp -r ./lib-scala/ ./dist/
--- runj.sh
+++ runj.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -classpath target/scala-2.10/name_2.10-0.1.jar:./lib/scala-library.jar org.Name "Encode this message!!"
+java -classpath target/scala-2.10/name_2.10-0.1.jar:./lib-scala/scala-library.jar org.Name "Encode this message!!"
